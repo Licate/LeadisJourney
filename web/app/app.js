b4b5b9aa07f7168d07ca77811649517f015df847
Manon Delanoy <manon.delanoy@epitech.eu>
2015-02-26 22:44:33 +0100

    First version

---
+++ web/app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Licate on 25/02/2015.
+ */
+var app = angular.module("leadis", ["ui.router", "ui.bootstrap"]);
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/");
+    $stateProvider.state("Home", {
+        url: "/",
+        templateUrl: "app/views/home.html",
+        controller: "homeCtrl"
+    })
+        .state("About", {
+            url: "/about",
+            templateUrl: "app/views/about.html"
+        });
+}]);
