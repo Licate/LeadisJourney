ced2b461da342b90ce944a88c37c50335e4741c0
Licate <manon.delanoy1@gmail.com>
2015-05-14 17:43:28 +0200

    base archi base jersey

---
+++ api/rest/src/main/java/org/leadisteam/leadisjourney/api/rest/resources/SampleResource.java
@@ -0,0 +1,14 @@
+package org.leadisteam.leadisjourney.api.rest.resources;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("Sample")
+@Produces(MediaType.APPLICATION_JSON)
+public class SampleResource {
+	@GET
+	public String sample()
+	{
+		return "Test";
+	}
+}
