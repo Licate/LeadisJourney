34298503198d0dce19f379422c51264ebec88356
Licate <manon.delanoy1@gmail.com>
2015-10-03 18:30:46 +0100

    setup maven project

---
+++ API/api/services/src/main/java/org/leadisteam/leadisjourney/api/services/App.java
@@ -0,0 +1,13 @@
+package org.leadisteam.leadisjourney.api.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
