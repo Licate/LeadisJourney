a2e31e6992020d11357d120a81d98037956d25ea
Licate <manon.delanoy1@gmail.com>
2016-01-07 12:44:45 +0100

    Repositories + new models

--- API/LeadisTeam.LeadisJourney.Api/src/LeadisTeam.LeadisJourney.Api/Controllers/AccountController.cs
+++ API/LeadisTeam.LeadisJourney.Api/src/LeadisTeam.LeadisJourney.Api/Controllers/AccountController.cs
@@ -1,54 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using LeadisTeam.LeadisJourney.Api.Models;
 using LeadisTeam.LeadisJourney.Core.Repositories;
 using LeadisTeam.LeadisJourney.Services.Contracts;
 using Microsoft.AspNet.Mvc;
 
 
-namespace LeadisTeam.LeadisJourney.Api.Controllers
-{
+namespace LeadisTeam.LeadisJourney.Api.Controllers {
     [Route("api/[controller]")]
-    public class AccountController : Controller
-    {
+    public class AccountController : Controller {
         private readonly IAccountService _accountService;
         private readonly IUnitOfWork _unitOfWork;
-        // GET: api/values
-	    public AccountController(IAccountService accountService, IUnitOfWork unitOfWork) {
-	        _accountService = accountService;
-	        _unitOfWork = unitOfWork;
-	    }
 
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
+        public AccountController(IAccountService accountService, IUnitOfWork unitOfWork) {
+            _accountService = accountService;
+            _unitOfWork = unitOfWork;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
-        {
+        public string Get(int id) {
             return "Je suis un account";
         }
 
-        // POST api/values
         [HttpPost]
-        public HttpOkResult Post([FromBody] CreateAccountModel res) {
+        public HttpOkResult Login([FromBody] LoginAccountModel res) {
+            throw new NotImplementedException();
+        }
+
+        // POST api/account/
+        [HttpPost]
+        public HttpOkResult Create([FromBody] CreateAccountModel res) {
             _accountService.Create(res.Pseudo, res.Email, res.Name, res.FirstName, res.Password);
             _unitOfWork.Commit();
             return Ok();
         }
 
-        // PUT api/values/5
+        // PUT api/account/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
+        public void Update(int id, [FromBody] UpdateAccountModel res) {
+            throw new NotImplementedException();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        public void Desactivate(int id) {
+            throw new NotImplementedException();
         }
     }
 }
